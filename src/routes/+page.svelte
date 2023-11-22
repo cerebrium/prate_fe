b3a81cad33fe90312f335d937325626699336c5a
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2023-11-21 16:27:07 -0800

    made some basic connection, not working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,47 @@
-export const prerender = true
-export const ssr = false
+<script>
+	import { onMount } from 'svelte';
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	let input_string = '';
+
+	// @ts-expect-error tbd
+	let conn;
+
+	function send_message() {
+		console.log('what is the message:', input_string);
+		input_string = '';
+
+		// @ts-expect-error tbd
+		conn.send(input_string);
+	}
+
+	onMount(() => {
+		try {
+			const connection = new WebSocket('ws://localhost:3000', 'utf-8');
+
+			const enc = new TextEncoder();
+			connection.send(enc.encode('hello'));
+
+			connection.send(enc.encode('hello'));
+
+			connection.addEventListener('message', (message) => {
+				input_string = message.toString();
+				console.log('incoming message from server: ', message);
+			});
+
+			return () => {
+				connection.close();
+			};
+		} catch (e) {
+			console.log('there was and error: ', e);
+		}
+	});
+</script>
+
+<div class="container">
+	<h1>Prate</h1>
+	<input type="text" bind:value={input_string} />
+	<button on:click={send_message}>Send Message</button>
+</div>
+
+<style>
+</style>
